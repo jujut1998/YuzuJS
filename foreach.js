20908c1ea59d92044848c07b6cd7267b245eb4a9
jujut1998 <jthomas2016@bklawtech.com>
2015-07-09 16:11:00 -0400

    Created forEach function

--- foreach.js
+++ foreach.js
@@ -1,24 +1,29 @@
 function log(text) {
-  var logEl = document.getElementById("log");
-  logEl.innerHTML += text + "<br />";
+	debugger;
+    var logEl = document.getElementById("log");
+    logEl.innerHTML += text + "<br />";
 }
 
+var itemCall = function (array) {    
+	var text = "";
+	for (i=0; i < array.length; i++) { 
+    	text += "<br />" + array[i] + "<br />";
+	}
+	return text;
+};
 // write a function `forEach` that takes 2 arguments
-function forEach(sports) {
-// 1) an array
-var sports = ["Football", "Soccer", "Basketball", "Volleyball", "Tennis", "Badminton"]
-
-var i = 0;
-var len = sports.length;
-var text = "";
-for (; i < len; i++) {
-  text += sports[i] + "<br>";
+function forEach(array, itemCall) {
+    return itemCall(array);
 }
+  
+var sports = ["Football", "Soccer", "Basketball", "Volleyball", "Tennis", "Badminton"];
 
-});
+log(forEach(sports, itemCall));
 
-}
+	// 1) an array
+	// 2) a function that will be called for each item in the array.
+	// this function has the same behavior as the Array forEach method.
 
-// this function has the same behavior as the Array forEach method.
-
-forEach();
+	// var colors = ["red", "green", "blue"];
+	// colors.forEach(myFunc)
+	// forEach(colors, myFunc)
