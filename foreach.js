a6ef98fb25e343776022ca3ba97a827876d4c3da
jujut1998 <jujut1998@users.noreply.github.com>
2015-07-09 14:35:51 -0400

    Update foreach.js

--- foreach.js
+++ foreach.js
@@ -1,4 +1,24 @@
+function log(text) {
+  var logEl = document.getElementById("log");
+  logEl.innerHTML += text + "<br />";
+}
+
 // write a function `forEach` that takes 2 arguments
+function forEach(sports) {
 // 1) an array
-// 2) a function that will be called for each item in the array.
+var sports = ["Football", "Soccer", "Basketball", "Volleyball", "Tennis", "Badminton"]
+
+var i = 0;
+var len = sports.length;
+var text = "";
+for (; i < len; i++) {
+  text += sports[i] + "<br>";
+}
+
+});
+
+}
+
 // this function has the same behavior as the Array forEach method.
+
+forEach();
