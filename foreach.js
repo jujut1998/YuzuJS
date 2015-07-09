d3492e8e442fdcc34133a2ae50bc16315e55b32f
Paul Bouzakis <github@paulbouzakis.com>
2015-07-09 13:05:11 -0400

    Add a foreach script.

---
+++ foreach.js
@@ -0,0 +1,4 @@
+// write a function `forEach` that takes 2 arguments
+// 1) an array
+// 2) a function that will be called for each item in the array.
+// this function has the same behavior as the Array forEach method.
